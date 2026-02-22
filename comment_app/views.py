270bd6c14fbf1efe480c9151d03bc6ba53b2d87c
aiden cullo <aidencullo@gmail.com>
2026-02-22 17:59:06 -0500

    starting from scratch

--- comment_app/views.py
+++
@@ -1,9 +0,0 @@
-# views.py
-from django.http import JsonResponse
-
-def hello(request):
-    data = [
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"}
-    ]
-    return JsonResponse(data, safe=False)
